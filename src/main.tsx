2601d554fa0a9d9ffa047e97d491fe777577f5a0
petrovdmitry <petrovdmitry@users.noreply.github.com>
2026-06-22 10:29:47 +0000

    Build: dist with index.html, assets, and questions.json

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import PrombezTrainer from "./App";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <PrombezTrainer />
-  </React.StrictMode>
-);
